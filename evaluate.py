f58af3acd76e8ad2d2608ba622e83cd32eb94f5c
James <j.mccouat@outlook.com>
2022-03-06 19:39:56 +0000

    fix tensor on cpu bug

--- evaluate.py
+++ evaluate.py
@@ -27,7 +27,7 @@ def get_eres(output_stack, predicted_points_scaled, pixel_sizes, significant_pix
     normalized_heatmaps = torch.div(output_stack, max_per_heatmap)
 
     filtered_heatmaps = torch.where(normalized_heatmaps > significant_pixel_cutoff, normalized_heatmaps,
-                                   torch.tensor(0.0))
+                                   torch.tensor(0.0).cuda())
     flattened_filtered_heatmaps = torch.flatten(filtered_heatmaps, start_dim=2)
     sum_per_heatmap = torch.sum(flattened_filtered_heatmaps, dim=2, keepdim=True)
     sum_per_heatmap = torch.unsqueeze(sum_per_heatmap, dim=3)
