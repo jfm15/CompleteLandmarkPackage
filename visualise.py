32c4ecda8f4758f6812e569f8485772fb15b26a8
James <j.mccouat@outlook.com>
2022-03-07 18:20:39 +0000

    extract visualise function

--- visualise.py
+++ visualise.py
@@ -1,114 +1,39 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def visualise_ensemble(images, output_per_model, target_points, eres_per_model, keypoint_idx):
+def visualise_aggregations(image, predicted_points_per_model, rec_weighted_model_points,
+                           mean_model_points, target_points, pixel_size, name, save_image_path):
 
-    s = 0
+    usable_image = np.squeeze(image) * 0.6
 
-    # Display image
-    image = images[s, 0]
+    # cycle through landmarks
+    no_of_landmarks = rec_weighted_model_points.shape[0]
+    for landmark_idx in range(no_of_landmarks):
 
-    plt.imshow(image, cmap='gray', vmin=0.0, vmax=255.0)
-    plt.show()
+        image_path = os.path.join(save_image_path, "{}_{}".format(name, landmark_idx + 1))
+        figure = plt.gcf()
+        figure.set_size_inches(usable_image.shape[1] / 50, usable_image.shape[0] / 50)
+        plt.imshow(usable_image, cmap='gray', vmin=0.0, vmax=255.0)
 
-    target_points = target_points[s]
-    output_per_modes = np.array(output_per_model).squeeze()
-    eres_per_model = np.array(eres_per_model).squeeze()
+        for model_idx in range(len(predicted_points_per_model)):
+            individual_model_pred = predicted_points_per_model[model_idx, landmark_idx]
+            scaled_individual_model_pred = np.divide(individual_model_pred, pixel_size)
+            plt.scatter(scaled_individual_model_pred[0], scaled_individual_model_pred[1], color='white', s=3)
 
-    models, keypoints, h, w = output_per_modes.shape
-    #for keypoint_idx in range(keypoints):
-    # show the heatmaps per model
+        mean_pred = mean_model_points[landmark_idx]
+        scaled_mean_pred = np.divide(mean_pred, pixel_size)
+        plt.scatter(scaled_mean_pred[0], scaled_mean_pred[1], color='violet', s=3)
 
-    relevant_heatmaps = output_per_modes[:, keypoint_idx]
-    eres_for_this_landmark = eres_per_model[:, keypoint_idx]
+        confidence_weighted_pred = rec_weighted_model_points[landmark_idx]
+        scaled_confidence_weighted_pred = np.divide(confidence_weighted_pred, pixel_size)
+        plt.scatter(scaled_confidence_weighted_pred[0], scaled_confidence_weighted_pred[1], color='red', s=3)
 
-    # show individual heatmaps
-    predicted_points = []
-    for model_idx, (model_heatmap, ere) in enumerate(zip(relevant_heatmaps, eres_for_this_landmark)):
-        print(model_idx, ere)
-        plt.imshow(image, cmap='gray', vmin=0.0, vmax=255.0)
-        plt.imshow(model_heatmap, cmap='inferno', alpha=0.4)
-        predicted_point = get_hottest_point(model_heatmap)
-        predicted_points.append(predicted_point)
-        # plt.text(predicted_point[0] + 5, predicted_point[1] + 5, "ERE: {:.2f}".format(ere), color='darkorange')
-        plt.scatter([predicted_point[0]], predicted_point[1], color='red', s=100)
-        # plt.text(predicted_point[0] - 5, predicted_point[1] - 5, "P{}".format(model_idx + 1), color='red')
-        # plt.arrow(predicted_point[0] - 5, predicted_point[1] - 5, 5, 5, shape='right', color='red')
-        plt.show()
+        ground_truth = target_points[landmark_idx]
+        scaled_ground_truth = np.divide(ground_truth, pixel_size)
+        plt.scatter(scaled_ground_truth[0], scaled_ground_truth[1], color='lime', s=3)
 
-    # show all predictions
-    predicted_points = np.array(predicted_points)
-    plt.imshow(image * 0.6, cmap='gray', vmin=0.0, vmax=255.0)
-    plt.scatter(predicted_points[:, 0], predicted_points[:, 1], color='red', s=100)
-    mean_point = np.mean(predicted_points, axis=0)
-    plt.scatter(mean_point[0], mean_point[1], color='violet', s=100)
-    rec_weighted_point = get_recp_weighted_point(eres_for_this_landmark, predicted_points)
-    plt.scatter(rec_weighted_point[0], rec_weighted_point[1], color='cyan', s=100)
-    plt.scatter(target_points[keypoint_idx, 0], target_points[keypoint_idx, 1], color='lime', s=100)
-    plt.show()
-
-    '''
-    predicted_points = []
-    for model_heatmap, color in zip(relevant_heatmaps, ['lightcoral', 'red', 'tomato']):
-        plt.imshow(model_heatmap, cmap='inferno', alpha=0.4)
-        plt.contour(model_heatmap / np.max(model_heatmap), levels=[0.2], colors=[color], zorder=2)
-        predicted_point = get_hottest_point(model_heatmap)
-        predicted_points.append(predicted_point)
-        plt.scatter([predicted_point[0]], predicted_point[1], color=color, marker='x', zorder=2)
-    #plt.imshow(squashed_heatmap, cmap='inferno', alpha=0.4)
-    predicted_points = np.array(predicted_points)
-    mean_point = np.mean(predicted_points, axis=0)
-    eres_for_this_landmark = eres_per_model[:, keypoint_idx]
-    sum_weighted_point = get_sum_weighted_point(eres_for_this_landmark, predicted_points)
-    max_weighted_point = get_max_weighted_point(eres_for_this_landmark, predicted_points)
-    rec_weighted_point = get_recp_weighted_point(eres_for_this_landmark, predicted_points)
-
-    #xs = [target_points[keypoint_idx, 0], mean_point[0], sum_weighted_point[0], max_weighted_point[0], rec_weighted_point[0]]
-    #ys = [target_points[keypoint_idx, 1], mean_point[1], sum_weighted_point[1], max_weighted_point[1], rec_weighted_point[1]]
-    xs = [target_points[keypoint_idx, 0], mean_point[0], rec_weighted_point[0]]
-    ys = [target_points[keypoint_idx, 1], mean_point[1], rec_weighted_point[1]]
-    plt.scatter(xs, ys, color=['g', 'g', 'r'], zorder=2)
-    plt.plot([target_points[keypoint_idx, 0], rec_weighted_point[0]],
-             [target_points[keypoint_idx, 1], rec_weighted_point[1]], color='yellow')
-    '''
-    # dist = np.linalg.norm(target_points[keypoint_idx] - rec_weighted_point)
-    # plt.text(target_points[keypoint_idx, 0], target_points[keypoint_idx, 1], "{:.2f}".format(dist), color="yellow", fontsize="small")
-
-    # plt.show()
-
-
-def visualise_ensemble_2(images, output_per_model, recp_points, target_points, eres_per_model):
-
-    recp_points = recp_points[0]
-    recp_points = np.divide(recp_points, np.array([0.2995426113372042, 0.2995426113372042]))
-
-    s = 0
-
-    # Display image
-    image = images[s, 0]
-
-    plt.imshow(image, cmap='gray', vmin=0.0, vmax=255.0)
-
-    target_points = target_points[s]
-    output_per_modes = np.array(output_per_model).squeeze()
-
-    relevant_heatmaps = output_per_modes[0]
-    squashed_heatmaps = np.max(relevant_heatmaps, axis=0)
-    plt.imshow(squashed_heatmaps, cmap='inferno', alpha=0.4)
-
-    for i, recp_point in enumerate(recp_points):
-        j = i + 1
-        offset = (5, 5)
-        if j == 11:
-            offset = (-30, 5)
-        if j == 8:
-            offset = (0, 20)
-        if j == 9:
-            offset = (5, 10)
-        plt.text(recp_point[0] + offset[0], recp_point[1] + offset[1], "{}".format(j), color="yellow", fontsize="small")
-
-    plt.scatter(target_points[:, 0], target_points[:, 1], color='lime', s=10)
-    plt.scatter(recp_points[:, 0], recp_points[:, 1], color='red', s=10)
-
-    plt.show()
+        plt.axis('off')
+        plt.savefig(image_path, bbox_inches='tight', dpi=100)
+        plt.close()
