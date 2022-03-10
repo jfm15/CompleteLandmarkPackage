c34856053d3ae2646f7d25e88479e3eed349c58e
James <j.mccouat@outlook.com>
2022-03-10 14:19:22 +0000

    refactoring

--- test.py
+++ test.py
@@ -1,19 +1,14 @@
 import argparse
 import torch
 import glob
-import os
-
-import numpy as np
 
 import model
-from model import two_d_softmax
-from model import nll_across_batch
 from landmark_dataset import LandmarkDataset
 from utils import prepare_for_testing
 from utils import get_validation_message
-from function import use_model
+from function import validate_ensemble
 from evaluate import cal_radial_errors
-from evaluate import get_confidence_weighted_points
+from evaluate import use_aggregate_methods
 from evaluate import get_sdr_statistics
 from torchsummary.torchsummary import summary_string
 
@@ -68,7 +63,6 @@ def main():
 
     # Load the testing dataset and put it into a loader
     test_dataset = LandmarkDataset(args.testing_images, args.annotations, cfg.DATASET, perform_augmentation=False)
-    #test_dataset.set_specific_image("cache/cephalometric/640_800/150.png")
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     # Load models and state dict from file
@@ -87,54 +81,21 @@ def main():
     logger.info(model_summary)
 
     logger.info("-----------Start Testing-----------")
-    avg_loss_per_model = []
-    predicted_points_per_model = []
-    eres_per_model = []
-    target_points = None
-    save_image_path = os.path.join(cfg.VALIDATION.SAVE_IMAGE_PATH, yaml_file_name)
-    if not os.path.exists(save_image_path):
-        os.makedirs(save_image_path)
+    predicted_points_per_model, eres_per_model, target_points = \
+        validate_ensemble(ensemble, test_loader, print_progress=True, logger=logger)
 
-    for model_idx in range(len(ensemble)):
-        logger.info('-----------Running Model {}-----------'.format(model_idx))
-        our_model = ensemble[model_idx]
-        our_model = our_model.cuda()
+    aggregated_point_dict = use_aggregate_methods(predicted_points_per_model, eres_per_model,
+                                                  aggregate_methods=cfg.VALIDATION.AGGREGATION_METHODS)
+    aggregated_point_mres = [cal_radial_errors(predicted_points, target_points) for
+                             predicted_points in aggregated_point_dict.values()]
 
-        all_losses, all_predicted_points, target_points, all_eres \
-            = use_model(our_model, two_d_softmax, test_loader, nll_across_batch,
-                        logger=logger, print_progress=True)
-
-        predicted_points_per_model.append(all_predicted_points)
-        eres_per_model.append(all_eres)
-
-        # radial_error_per_model.append(all_radial_errors)
-        avg_loss_per_model.append(all_losses)
-
-        # move model back to cpu
-        our_model.cpu()
-
-    predicted_points_per_model = torch.stack(predicted_points_per_model)
-    eres_per_model = torch.stack(eres_per_model)
-    # predicted_points_per_model is size [M, D, N, 2]
-    # eres_per_model is size [M, D, N]
-    # target_points is size [D, N, 2]
-
-    # perform analysis
-    per_model_mre = [cal_radial_errors(predicted_points, target_points, mean=True)
-                     for predicted_points in predicted_points_per_model]
-
-    mean_aggregation_points = torch.mean(predicted_points_per_model, dim=0)
-    mean_aggregation_mre = cal_radial_errors(mean_aggregation_points, target_points, mean=True)
-
-    confidence_weighted_points = get_confidence_weighted_points(predicted_points_per_model, eres_per_model)
-    confidence_weighted_errors = cal_radial_errors(confidence_weighted_points, target_points)
-    confidence_weighted_mre = torch.mean(confidence_weighted_errors).item()
-
-    sdr_statistics = get_sdr_statistics(confidence_weighted_errors, cfg.VALIDATION.SDR_THRESHOLDS)
+    # get sdr for confidence weighted predictions
+    sdr_statistics = get_sdr_statistics(aggregated_point_dict[cfg.VALIDATION.SDR_AGGREGATION_METHOD],
+                                        cfg.VALIDATION.SDR_THRESHOLDS)
 
     logger.info('-----------Overall Statistics-----------')
-    msg = get_validation_message(per_model_mre, mean_aggregation_mre, confidence_weighted_mre,
-                                 cfg.VALIDATION.SDR_THRESHOLDS, sdr_statistics)
+    msg = get_validation_message(aggregated_point_mres, cfg.TRAIN.ENSEMBLE_MODELS, cfg.VALIDATION.AGGREGATION_METHODS,
+                                 cfg.VALIDATION.SDR_AGGREGATION_METHOD, cfg.VALIDATION.SDR_THRESHOLDS, sdr_statistics)
     logger.info(msg)
 
 
