069f6c7428c4e32d8a76d7f61f92350f73c2cd2a
James <j.mccouat@outlook.com>
2022-10-14 16:04:28 +0100

    add u-net model

--- lib/models/models.py
+++ lib/models/models.py
@@ -21,6 +21,22 @@ class UnetPlusPlus(nn.Module):
         return self.unet(x)
 
 
+class Unet(nn.Module):
+    def __init__(self, cfg_model, no_of_landmarks):
+        super(Unet, self).__init__()
+        self.unet = smp.Unet(
+            encoder_name=cfg_model.ENCODER_NAME,
+            encoder_weights=cfg_model.ENCODER_WEIGHTS,
+            decoder_channels=cfg_model.DECODER_CHANNELS,
+            in_channels=cfg_model.IN_CHANNELS,
+            classes=no_of_landmarks,
+        )
+        self.temperatures = nn.Parameter(torch.ones(1, no_of_landmarks, 1, 1), requires_grad=False)
+
+    def forward(self, x):
+        return self.unet(x)
+
+
 def two_d_softmax(x):
     exp_y = torch.exp(x)
     return exp_y / torch.sum(exp_y, dim=(2, 3), keepdim=True)
