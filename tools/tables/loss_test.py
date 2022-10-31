ba331a45cd3b597fbe9598fce095e434c9397a4d
James <j.mccouat@outlook.com>
2022-10-31 12:42:14 +0000

    fix aug parameters

--- tools/tables/loss_test.py
+++ tools/tables/loss_test.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-bce_ceph_results = np.array([1.486, 1.494, 1.454])
-bce_hand_results = np.array([0.667, 0.679, 0.685])
-bce_pelvis_results = np.array([2.343, 2.253, 2.328])
-bce_ultra_results = np.array([6.593, 7.114, 6.848])
+bce_ceph_results = np.array([1.430, 1.375, 1.382])
+bce_hand_results = np.array([0.647, 0.646, 0.650])
+bce_pelvis_results = np.array([2.431, 2.154, 2.380])
+bce_ultra_results = np.array([6.886, 6.613, 6.977])
 
 
 def print_stats(arr):
