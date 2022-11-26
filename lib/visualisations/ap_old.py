963486c3fe67759671a4ec635f22df33161d2d11
James <j.mccouat@outlook.com>
2022-11-26 23:24:56 +0000

    add better visualisations

--- lib/visualisations/ap_old.py
+++ lib/visualisations/ap_old.py
@@ -1,48 +1,51 @@
 
-
-def alpha_angles(ax, predicted_points, target_points):
-
-    right_center = target_points[1]
-    right_cam_point = target_points[6]
-    right_neck_point = target_points[2]
-
-    left_center = target_points[4]
-    left_cam_point = target_points[7]
-    left_neck_point = target_points[5]
+def alpha_angles(ax, points, color):
+    _, right_center, right_neck_point, _, left_center, left_neck_point, right_cam_point, left_cam_point = points
 
     # left
-    ax.plot([left_center[0], left_cam_point[0]], [left_center[1], left_cam_point[1]], color='lime')
-    ax.plot([left_center[0], left_neck_point[0]], [left_center[1], left_neck_point[1]], color='lime')
+    ax.plot([left_center[0], left_cam_point[0]], [left_center[1], left_cam_point[1]], color=color)
+    ax.plot([left_center[0], left_neck_point[0]], [left_center[1], left_neck_point[1]], color=color)
 
     # right
-    ax.plot([right_center[0], right_cam_point[0]], [right_center[1], right_cam_point[1]], color='lime')
-    ax.plot([right_center[0], right_neck_point[0]], [right_center[1], right_neck_point[1]], color='lime')
+    ax.plot([right_center[0], right_cam_point[0]], [right_center[1], right_cam_point[1]], color=color)
+    ax.plot([right_center[0], right_neck_point[0]], [right_center[1], right_neck_point[1]], color=color)
 
+
+def compare_alpha_angles(ax, predicted_points, target_points):
+
+    alpha_angles(ax, target_points, "lime")
+    alpha_angles(ax, predicted_points, "red")
+
+    '''
     ax.scatter([right_center[0], right_cam_point[0], right_neck_point[0], left_center[0], left_cam_point[0], left_neck_point[0]],
                [right_center[1], right_cam_point[1], right_neck_point[1], left_center[1], left_cam_point[1], left_neck_point[1]],
                color='green', s=20)
-
+    '''
 
     #for i, positions in enumerate(target_points):
     #    idx = important_indices[i]
     #    ax.text(positions[0], positions[1], "{}".format(idx + 1), color="yellow", fontsize="small")
 
 
-def lce_angles(ax, predicted_points, target_points):
+def lce_angles(ax, points, color):
 
-    right_center = target_points[1]
-    right_lat_point = target_points[0]
-
-    left_center = target_points[4]
-    left_lat_point = target_points[3]
+    right_lat_point, right_center, _, left_lat_point, left_center, _, _, _ = points
 
     # left
-    ax.plot([left_center[0], left_lat_point[0]], [left_center[1], left_lat_point[1]], color='lime')
-    ax.plot([left_center[0], left_center[0]], [left_center[1], left_center[1] - 50], color='lime')
+    ax.plot([left_center[0], left_lat_point[0]], [left_center[1], left_lat_point[1]], color=color)
+    ax.plot([left_center[0], left_center[0]], [left_center[1], left_center[1] - 50], color=color)
 
     # right
-    ax.plot([right_center[0], right_lat_point[0]], [right_center[1], right_lat_point[1]], color='lime')
-    ax.plot([right_center[0], right_center[0]], [right_center[1], right_center[1] - 50], color='lime')
+    ax.plot([right_center[0], right_lat_point[0]], [right_center[1], right_lat_point[1]], color=color)
+    ax.plot([right_center[0], right_center[0]], [right_center[1], right_center[1] - 50], color=color)
 
+
+def compare_lce_angles(ax, predicted_points, target_points):
+
+    lce_angles(ax, target_points, "lime")
+    lce_angles(ax, predicted_points, "red")
+
+    '''
     ax.scatter([right_center[0], right_lat_point[0], left_center[0], left_lat_point[0]],
                [right_center[1], right_lat_point[1], left_center[1], left_lat_point[1]], color='green', s=20)
+    '''
