02930312417f5b28002b7569ff39074b2bfc72ed
James <j.mccouat@outlook.com>
2022-12-28 23:39:51 +0000

    add support for extra pelvis dataset

--- lib/visualisations/ap.py
+++ lib/visualisations/ap.py
@@ -5,11 +5,11 @@ from lib.utils import get_center_of_circle
 # These are here because they can be called from other files
 
 
-def alpha_angles(ax, predicted_points, target_points):
+def alpha_angles(ax, points, color):
 
     important_indices = [2, 3, 10, 11, 12, 17, 18, 25, 26, 27]
-    predicted_points = np.take(predicted_points, important_indices, axis=0)
-    target_points = np.take(target_points, important_indices, axis=0)
+    predicted_points = np.take(points, important_indices, axis=0)
+    #target_points = np.take(target_points, important_indices, axis=0)
 
     #ax.scatter(predicted_points[:, 0], predicted_points[:, 1], color='red', s=5)
     #ax.scatter(target_points[:, 0], target_points[:, 1], color='green', s=5)
@@ -24,12 +24,12 @@ def alpha_angles(ax, predicted_points, target_points):
     centers = np.array(centers)
 
     # left
-    ax.plot([centers[0, 0], predicted_points[0, 0]], [centers[0, 1], predicted_points[0, 1]], color='red')
-    ax.plot([centers[0, 0], predicted_points[1, 0]], [centers[0, 1], predicted_points[1, 1]], color='red')
+    ax.plot([centers[0, 0], predicted_points[0, 0]], [centers[0, 1], predicted_points[0, 1]], color=color)
+    ax.plot([centers[0, 0], predicted_points[1, 0]], [centers[0, 1], predicted_points[1, 1]], color=color)
 
     # right
-    ax.plot([centers[1, 0], predicted_points[5, 0]], [centers[1, 1], predicted_points[5, 1]], color='red')
-    ax.plot([centers[1, 0], predicted_points[6, 0]], [centers[1, 1], predicted_points[6, 1]], color='red')
+    ax.plot([centers[1, 0], predicted_points[5, 0]], [centers[1, 1], predicted_points[5, 1]], color=color)
+    ax.plot([centers[1, 0], predicted_points[6, 0]], [centers[1, 1], predicted_points[6, 1]], color=color)
 
 
     #for i, positions in enumerate(target_points):
@@ -37,22 +37,22 @@ def alpha_angles(ax, predicted_points, target_points):
     #    ax.text(positions[0], positions[1], "{}".format(idx + 1), color="yellow", fontsize="small")
 
 
-def lce_angles(ax, predicted_points, target_points):
+def lce_angles(ax, points, color):
 
     centers = []
     for i in [10, 25]:
-        centers.append(get_center_of_circle(predicted_points[i],
-                                            predicted_points[i + 1],
-                                            predicted_points[i + 2]).numpy())
+        centers.append(get_center_of_circle(points[i],
+                                            points[i + 1],
+                                            points[i + 2]).numpy())
     centers = np.array(centers)
 
     # left
-    ax.plot([centers[0, 0], predicted_points[0, 0]], [centers[0, 1], predicted_points[0, 1]], color='red')
-    ax.plot([centers[0, 0], centers[0, 0]], [centers[0, 1], centers[0, 1] - 50], color='red')
+    ax.plot([centers[0, 0], points[0, 0]], [centers[0, 1], points[0, 1]], color=color)
+    ax.plot([centers[0, 0], centers[0, 0]], [centers[0, 1], centers[0, 1] - 50], color=color)
 
     # right
-    ax.plot([centers[1, 0], predicted_points[15, 0]], [centers[1, 1], predicted_points[15, 1]], color='red')
-    ax.plot([centers[1, 0], centers[1, 0]], [centers[1, 1], centers[1, 1] - 50], color='red')
+    ax.plot([centers[1, 0], points[15, 0]], [centers[1, 1], points[15, 1]], color=color)
+    ax.plot([centers[1, 0], centers[1, 0]], [centers[1, 1], centers[1, 1] - 50], color=color)
 
 
 def neck_shaft_angles(ax, predicted_points, target_points):
@@ -89,3 +89,15 @@ def acetabular_indices(ax, predicted_points, target_points):
             [predicted_points[15, 1], predicted_points[16, 1]], color='red')
     ax.plot([predicted_points[16, 0], predicted_points[16, 0] - 50],
             [predicted_points[16, 1], predicted_points[16, 1]], color='red')
+
+
+def compare_alpha_angles(ax, predicted_points, target_points):
+
+    alpha_angles(ax, target_points, "lime")
+    alpha_angles(ax, predicted_points, "red")
+
+
+def compare_lce_angles(ax, predicted_points, target_points):
+
+    lce_angles(ax, target_points, "lime")
+    lce_angles(ax, predicted_points, "red")
